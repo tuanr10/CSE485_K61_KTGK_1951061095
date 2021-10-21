14bf45194caa90037f60212b56a655a3074e58c5
tuanr10 <tuanmanucian2001@gmail.com>
2021-10-21 12:36:41 +0700

    Update nguoi_nhan_mau.sql

--- Database/nguoi_nhan_mau.sql
+++ Database/nguoi_nhan_mau.sql
@@ -34,7 +34,7 @@ CREATE TABLE `nguoi_nhan_mau` (
   `reci_bgrp` char(255) COLLATE utf8_unicode_ci DEFAULT NULL,
   `reci_bqnty` char(200) COLLATE utf8_unicode_ci DEFAULT NULL,
   `reci_sex` varchar(10) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `reci_reg_date,` date DEFAULT NULL,
+  `reci_reg_date` date DEFAULT NULL,
   `reci_phno` char(50) COLLATE utf8_unicode_ci DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
@@ -42,7 +42,7 @@ CREATE TABLE `nguoi_nhan_mau` (
 -- Đang đổ dữ liệu cho bảng `nguoi_nhan_mau`
 --
 
-INSERT INTO `nguoi_nhan_mau` (`reci_id`, `reci_name`, `reci_age`, `reci_bgrp`, `reci_bqnty`, `reci_sex`, `reci_reg_date,`, `reci_phno`) VALUES
+INSERT INTO `nguoi_nhan_mau` (`reci_id`, `reci_name`, `reci_age`, `reci_bgrp`, `reci_bqnty`, `reci_sex`, `reci_reg_date`, `reci_phno`) VALUES
 (1, 'Đỗ Quang Tuấn', 20, 'AB', '300ml', 'Nam', '2021-10-15', '0988888888'),
 (2, 'Đỗ Quang Huấn', 29, 'A', '400ml', 'Nam', '2021-10-16', '0988898888');
 
